32064a23d5f425c2a17b6fee790a4e4c4a09afa0
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-04-21 12:24:19 -0400

    first commit

---
+++ src/components/MovieItem.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { createImageUrl } from "../services/movieServices";
+
+const MovieItem = ({ movie }) => {
+  const { title, backdrop_path } = movie;
+  return (
+    <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2">
+      <img src={createImageUrl(backdrop_path, "w500")} alt={movie.title} />
+    </div>
+  );
+};
+
+export default MovieItem;
